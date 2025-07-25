7fb857a7fb92b40c61e0e4d6820cafbc32e6f17e
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-25 20:45:13 -0300

    remove unecessary scaffold from widget

--- lib/presentation/widgets/new_todo_modal.dart
+++ lib/presentation/widgets/new_todo_modal.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
 class NewTodoModal extends StatefulWidget {
-  
-  const NewTodoModal({
-  super.key, 
-  required this.title,
-  required this.onSave,
-  });
-  
+  const NewTodoModal({super.key, required this.title, required this.onSave});
+
   final String title;
   final void Function(String text) onSave;
 
@@ -17,44 +12,39 @@ class NewTodoModal extends StatefulWidget {
 
 class _NewTodoModalState extends State<NewTodoModal> {
   final TextEditingController _newTodoModalController = TextEditingController();
-  
+
   void _saveTodo() {
-  final text = _newTodoModalController.text.trim();
-  if (text.isNotEmpty) {
-    widget.onSave(text);
+    final text = _newTodoModalController.text.trim();
+    if (text.isNotEmpty) {
+      widget.onSave(text);
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          width: 300,
-          height: 600,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
+    return Container(
+      width: 300,
+      height: 600,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        children: [
+          Text(
+            "Criar novo todo",
+            style: TextStyle(fontSize: 36, color: Colors.black),
           ),
-          child: Column(
-            children: [
-              Text(
-                "Criar novo todo",
-                style: TextStyle(fontSize: 36, color: Colors.black),
-              ),
-              TextField(
-                controller: _newTodoModalController,
-                decoration: InputDecoration(
-                  hintText: "Digite seu novo todo",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              ElevatedButton(onPressed: _saveTodo, child: Text("Salvar")),
-            ],
+          TextField(
+            controller: _newTodoModalController,
+            decoration: InputDecoration(
+              hintText: "Digite seu novo todo",
+              border: OutlineInputBorder(),
+            ),
           ),
-        ),
-      )
+          ElevatedButton(onPressed: _saveTodo, child: Text("Salvar")),
+        ],
+      ),
     );
   }
 }
