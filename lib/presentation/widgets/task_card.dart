a5fcdd190a65cc647ef1a613cddfa1af9a49ee62
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-08-10 19:46:25 -0300

    improve todo card layout

--- lib/presentation/widgets/task_card.dart
+++ lib/presentation/widgets/task_card.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/domain/entities/todo_item.dart';
 
-class Todo {
-  final String text;
-  final bool value;
-  Todo({required this.text, required this.value});
-}
-
 class TodoItemCard extends StatelessWidget {
   const TodoItemCard({
     super.key,
@@ -21,19 +15,64 @@ class TodoItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Checkbox(
-          value: todo.value,
-          onChanged: (bool? newValue) {
-            if (newValue != null) {
-              onChanged(index, newValue);
-            }
-          },
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              // 🟢 Main content takes all available horizontal space
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: todo.value,
+                          onChanged: (bool? newValue) {
+                            if (newValue != null) {
+                              onChanged(index, newValue);
+                            }
+                          },
+                        ),
+                        Expanded(
+                          child: Text(
+                            todo.text,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      "New todo created",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                    const Text(
+                      "Generic tasks",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+              ),
+
+              Text(
+                '7:00',
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
         ),
-        Text(todo.text, style: const TextStyle(color: Colors.white)),
-      ],
+      ),
     );
   }
 }
