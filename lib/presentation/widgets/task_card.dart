e45aa38317401127ff9a7db6977bdb32b8bf86a3
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-08-10 21:21:55 -0300

    split task card widget into small private widgets

--- lib/presentation/widgets/task_card.dart
+++ lib/presentation/widgets/task_card.dart
@@ -1,6 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/domain/entities/todo_item.dart';
 
+class _TodoHeader extends StatelessWidget {
+  const _TodoHeader({
+    required this.onChanged,
+    required this.todo,
+    required this.index,
+  });
+
+  final void Function(int todoIndex, bool newValue) onChanged;
+  final TodoItem todo;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value: todo.value,
+          onChanged: (bool? newValue) {
+            if (newValue != null) {
+              onChanged(index, newValue);
+            }
+          },
+        ),
+        Expanded(
+          child: Text(
+            todo.text,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        Text(
+          '7:00',
+          style: TextStyle(color: Colors.grey.shade600, fontSize: 14),
+        ),
+      ],
+    );
+  }
+}
+
+class _TodoCategory extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Row(
+      children: [
+        Container(
+          width: 10,
+          height: 10,
+          decoration: const BoxDecoration(
+            color: Colors.red,
+            shape: BoxShape.circle,
+          ),
+        ),
+        SizedBox(width: 10),
+        const Text(
+          "Generic",
+          style: TextStyle(color: Colors.grey, fontSize: 12),
+        ),
+      ],
+    );
+  }
+}
+
+class _TodoContent extends StatelessWidget {
+  const _TodoContent({
+    required this.onChanged,
+    required this.todo,
+    required this.index,
+  });
+
+  final void Function(int todoIndex, bool newValue) onChanged;
+  final TodoItem todo;
+  final int index;
+  @override
+  Widget build(Object context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _TodoHeader(onChanged: onChanged, todo: todo, index: index),
+              const Text(
+                "New todo created",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+              _TodoCategory(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class TodoItemCard extends StatelessWidget {
   const TodoItemCard({
     super.key,
@@ -15,80 +112,10 @@ class TodoItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(30),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: todo.value,
-                              onChanged: (bool? newValue) {
-                                if (newValue != null) {
-                                  onChanged(index, newValue);
-                                }
-                              },
-                            ),
-                            Expanded(
-                              child: Text(
-                                todo.text,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            Text(
-                              '7:00',
-                              style: TextStyle(
-                                color: Colors.grey.shade600,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Text(
-                          "New todo created",
-                          style: TextStyle(color: Colors.grey, fontSize: 12),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 10,
-                          height: 10,
-                          decoration: const BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        const Text(
-                          "Generic",
-                          style: TextStyle(color: Colors.grey, fontSize: 12),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return 
+    Card(child: Padding(
+      padding: const EdgeInsets.all(15),
+      child: _TodoContent(onChanged: onChanged, todo: todo, index: index),
+    ),);
   }
 }
