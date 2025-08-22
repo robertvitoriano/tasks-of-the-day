b215290b6f3caae6f85bc81fe43d587776594e76
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-08-21 22:52:46 -0300

    ensure today list always exist

--- lib/notifiers/day_lists_notifier.dart
+++ lib/notifiers/day_lists_notifier.dart
@@ -6,13 +6,33 @@ import 'package:uuid/uuid.dart';
 final _uuid = Uuid();
 
 class DayListsNotifier extends Notifier<List<DayList>> {
+  DayListsNotifier() : super() {
+    if (!_hasDayListForToday(state)) {
+      addTodayDayList();
+    }
+  }
+  bool _hasDayListForToday(List<DayList> dayLists) {
+    final today = DateTime.now();
+    return dayLists.any((list) {
+      final listDate = DateTime.parse(list.title);
+      return listDate.year == today.year &&
+          listDate.month == today.month &&
+          listDate.day == today.day;
+    });
+  }
+
   @override
   List<DayList> build() => [];
 
   void addTodayDayList() {
     state = [
       ...state,
-      DayList(id: _uuid.v4(), title: DateTime.now().toIso8601String().split('T')[0], tasks: [], date: DateTime.now()),
+      DayList(
+        id: _uuid.v4(),
+        title: DateTime.now().toIso8601String().split('T')[0],
+        tasks: [],
+        date: DateTime.now(),
+      ),
     ];
   }
 
@@ -26,7 +46,12 @@ class DayListsNotifier extends Notifier<List<DayList>> {
         return list.copyWith(
           tasks: [
             ...list.tasks,
-            Task(id: _uuid.v4(), dayListId: dayListId, title: taskTitle, done: false),
+            Task(
+              id: _uuid.v4(),
+              dayListId: dayListId,
+              title: taskTitle,
+              done: false,
+            ),
           ],
         );
       }
